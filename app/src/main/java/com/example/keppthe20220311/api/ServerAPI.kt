07115496988d4cbf1ea40e256184d6e90516201b
Administrator <naki5087@naver.com>
2022-03-11 15:42:33 +0900

    JSON 파싱을 쉽게 도와주는 GSON 라이브러리 결합

--- app/src/main/java/com/example/keppthe20220311/api/ServerAPI.kt
+++ app/src/main/java/com/example/keppthe20220311/api/ServerAPI.kt
@@ -1,6 +1,7 @@
 package com.example.keppthe20220311.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ServerAPI {
 
@@ -18,10 +19,12 @@ class ServerAPI {
        fun getRetrofit() : Retrofit {
 
 //           Retrofit 라이브러리는 클래스 차원에서 BASE_URL을 설정할 수 있게 도와줌
+//           Retrofit + Gson 두개의 라이브러리를 결합하면 =>  JSON 파싱이 쉬워짐.
 
            if(retrofit == null){
                retrofit = Retrofit.Builder()
                    .baseUrl(BASE_URL) //어느 서버를 기반으로 움직일건지.
+                   .addConverterFactory(GsonConverterFactory.create()) //gson 라이브러리와 결합
                    .build()
            }
            return retrofit!!
