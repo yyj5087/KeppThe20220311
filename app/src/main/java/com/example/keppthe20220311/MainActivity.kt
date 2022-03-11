bc6ee34a58ee92382ea2310cf6c106e1964d13fc
Administrator <naki5087@naver.com>
2022-03-11 14:47:04 +0900

    메인화면  BaseActivity   상속

--- app/src/main/java/com/example/keppthe20220311/MainActivity.kt
+++ app/src/main/java/com/example/keppthe20220311/MainActivity.kt
@@ -5,10 +5,20 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.keppthe20220311.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
