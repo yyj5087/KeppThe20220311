1834f6d0b885176c2bfe9e67d33ae7c3c2c6c6c3
Administrator <naki5087@naver.com>
2022-03-11 16:17:15 +0900

    로그인 함수 호출 > 서버 기능 Call
    - 임시  Callback 등록: 응답 내용만 확인

--- app/src/main/java/com/example/keppthe20220311/SignInActivity.kt
+++ app/src/main/java/com/example/keppthe20220311/SignInActivity.kt
@@ -2,8 +2,15 @@ package com.example.keppthe20220311
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import com.example.keppthe20220311.api.APIList
+import com.example.keppthe20220311.api.ServerAPI
 import com.example.keppthe20220311.databinding.ActivitySignInBinding
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignInActivity : BaseActivity() {
     lateinit var binding: ActivitySignInBinding
@@ -15,10 +22,27 @@ class SignInActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        binding.btnLogin.setOnClickListener {
 
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPassword = binding.edtPassword.text.toString()
+
+            apiList.postRequestLogin(inputEmail, inputPassword).enqueue(object : Callback<JSONObject> {
+                override fun onResponse(call: Call<JSONObject>, response: Response<JSONObject>) {
+                    Log.d("응답확인", response.toString())
+                }
+
+                override fun onFailure(call: Call<JSONObject>, t: Throwable) {
+//                    서버에 물리적 연결 실패.
+                }
+
+
+                })
+        }
     }
 
     override fun setValues() {
 
     }
+
 }
--- app/src/main/java/com/example/keppthe20220311/api/APIList.kt
+++ app/src/main/java/com/example/keppthe20220311/api/APIList.kt
@@ -1,4 +1,19 @@
 package com.example.keppthe20220311.api
 
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface APIList {
+
+//    BASE_URL에 해당하는 서버에서, 어떤 기능들을 사용할건지 명시.
+
+    @FormUrlEncoded //파라미터중에, Field (formData)에 담아야하는 파라미터가 있ekaus 필요한 문구
+    @POST("/user")
+    fun postRequestLogin(
+    @Field("email") email: String,
+    @Field("password") pw: String
+    ) : Call<JSONObject>
 }
