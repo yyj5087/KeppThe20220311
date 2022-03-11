07bda1b8f9fc38a0959db22e67d2382f9da92ae2
Administrator <naki5087@naver.com>
2022-03-11 16:20:53 +0900

    응답 성공 /실패 분기 나누기

--- app/src/main/java/com/example/keppthe20220311/SignInActivity.kt
+++ app/src/main/java/com/example/keppthe20220311/SignInActivity.kt
@@ -30,6 +30,19 @@ class SignInActivity : BaseActivity() {
             apiList.postRequestLogin(inputEmail, inputPassword).enqueue(object : Callback<JSONObject> {
                 override fun onResponse(call: Call<JSONObject>, response: Response<JSONObject>) {
                     Log.d("응답확인", response.toString())
+
+                    //Retrofit 라이브러리의 response는 성공/실패 여부에 따라 다른 본문을 봐야함
+
+//                    성공인지?
+                    if(response.isSuccessful){
+//                        모든 결과가 최종 성공인 경우., (code = 200으로 내려옴)
+//                        response.body() 이용
+                    }
+//                    실패일때는?
+                    else{
+//                        서버 연결은 되었는데 로직만 실패 (로그인 비번 틀림)
+//                        response.errorBody() 활용
+                    }
                 }
 
                 override fun onFailure(call: Call<JSONObject>, t: Throwable) {
