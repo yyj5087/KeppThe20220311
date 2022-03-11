b6cbd753777b19f9edac0278664ff85a55975a84
Administrator <naki5087@naver.com>
2022-03-11 15:06:04 +0900

    임시 =스플래쉬 화면에서 2.5초후에 무조건 메인으로

--- app/src/main/java/com/example/keppthe20220311/SplashActivity.kt
+++ app/src/main/java/com/example/keppthe20220311/SplashActivity.kt
@@ -1,11 +1,33 @@
 package com.example.keppthe20220311
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+//        임시 - 자동 로그인 구현 전까지는, 2.5초 후에 무조건 로그인화면으로.
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+          val myIntent = Intent(mContext, SignInActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+        },2500)
     }
 }
