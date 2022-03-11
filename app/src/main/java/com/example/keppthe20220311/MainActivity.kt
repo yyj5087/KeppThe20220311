e83b09d582a1f07d54d2846cbce8378cbeebfe25
Administrator <naki5087@naver.com>
2022-03-11 14:43:07 +0900

    메인화면 바인딩 처리

---
+++ app/src/main/java/com/example/keppthe20220311/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.keppthe20220311
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.keppthe20220311.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
