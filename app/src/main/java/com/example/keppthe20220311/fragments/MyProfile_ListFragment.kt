36b9633e5611d352add10fc91c06e54a1bd5a59f
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-14 12:59:58 +0900

    내 프로필 프래그먼트의 데이터 바인딩 세팅

--- app/src/main/java/com/example/keppthe20220311/fragments/MyProfile_ListFragment.kt
+++ app/src/main/java/com/example/keppthe20220311/fragments/MyProfile_ListFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.keppthe20220311.R
+import com.example.keppthe20220311.databinding.FragmentMyProfileBinding
 import com.example.keppthe20220311.datas.BasicResponse
 import com.example.keppthe20220311.utils.ContextUtil
 import retrofit2.Call
@@ -13,12 +15,16 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MyProfile_ListFragment : BaseFragment() {
+
+    lateinit var binding: FragmentMyProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_profile,container,false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_my_profile,container,false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -39,6 +45,10 @@ class MyProfile_ListFragment : BaseFragment() {
         apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext)).enqueue(object :Callback<BasicResponse>{
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
 
+                if(response.isSuccessful){
+                    val br = response.body()!!
+  //                  br.data.user.nick_name // 프래그먼트의 txtNickname은 어떻게 가져와야하는가?
+                }
             }
 
             override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
