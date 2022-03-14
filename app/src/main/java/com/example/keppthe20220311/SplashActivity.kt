16d2242dd5af35d432838edb158379d1152f2177
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-14 13:27:03 +0900

    자동로그인 기능 구현 완료  - 임시 : 내 정보 조회 성공 여부만 체크

--- app/src/main/java/com/example/keppthe20220311/SplashActivity.kt
+++ app/src/main/java/com/example/keppthe20220311/SplashActivity.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.example.keppthe20220311.datas.BasicResponse
+import com.example.keppthe20220311.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +26,39 @@ class SplashActivity : BaseActivity() {
     override fun setValues() {
 //        임시 - 자동 로그인 구현 전까지는, 2.5초 후에 무조건 로그인화면으로.
 
+        var isMyInfoLoaded = false
+
+        apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext))
+            .enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+//                code : 200 => 성공 응답이 왔다? 내정보가 일단 잘 불러졌다.
+                    if (response.isSuccessful) {
+                        isMyInfoLoaded = true
+                    }
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
+
         val myHandler = Handler(Looper.getMainLooper())
         myHandler.postDelayed({
 
-          val myIntent = Intent(mContext, SignInActivity::class.java)
-            startActivity(myIntent)
+            val myIntent: Intent
 
+            if (isMyInfoLoaded) {
+                myIntent = Intent(mContext, MainActivity::class.java)
+            } else {
+                myIntent = Intent(mContext, SignInActivity::class.java)
+            }
+            startActivity(myIntent)
             finish()
-        },2500)
+
+        }, 2500)
     }
 }
--- app/src/main/java/com/example/keppthe20220311/fragments/MyProfile_ListFragment.kt
+++ app/src/main/java/com/example/keppthe20220311/fragments/MyProfile_ListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.keppthe20220311.R
 import com.example.keppthe20220311.api.ServerAPI
 import com.example.keppthe20220311.databinding.FragmentMyProfileBinding
@@ -50,6 +51,8 @@ class MyProfile_ListFragment : BaseFragment() {
                 if(response.isSuccessful){
                     val br = response.body()!!
                    binding.txtNickname.text = br.data.user.nick_name // 프래그먼트의 txtNickname은 어떻게 가져와야하는가?
+
+                    Glide.with(mContext).load(br.data.user.profile_img).into(binding.imgProfile)
                 }
             }
 
