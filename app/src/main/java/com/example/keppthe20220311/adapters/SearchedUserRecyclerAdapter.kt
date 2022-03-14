cea8be97896ff6d302cdcc76c6a5c0a14baf36c1
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-14 17:32:19 +0900

    친구 목록 접속 오류

--- app/src/main/java/com/example/keppthe20220311/adapters/SearchedUserRecyclerAdapter.kt
+++ app/src/main/java/com/example/keppthe20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -1,11 +1,24 @@
 package com.example.keppthe20220311.adapters
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class SearchedUserRecyclerAdapter : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
