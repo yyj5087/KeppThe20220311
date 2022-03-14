8acadcae7a80ae17826ca3fc95bbda62347bab8f
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-14 15:04:19 +0900

    리스트뷰 어댑터 뼈대작업 마무리

--- app/src/main/java/com/example/keppthe20220311/adapters/MyFriendsAdapter.kt
+++ app/src/main/java/com/example/keppthe20220311/adapters/MyFriendsAdapter.kt
@@ -2,11 +2,25 @@ package com.example.keppthe20220311.adapters
 
 import android.content.Context
 import android.service.autofill.UserData
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.keppthe20220311.R
 
 class MyFriendsAdapter(
     val mContext: Context,
     resId: Int,
     val mList: List<UserData>
 ) : ArrayAdapter<UserData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.activity_manage_my_friends, null)
+        }
+        val row = tempRow!!
+        return row
+    }
 }
