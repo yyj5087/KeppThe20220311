1a4ffbe479439fb1947ea5f75b450ffc0ebf7738
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-14 13:14:49 +0900

    연습문제 -  약속목록프래그먼트에 BaseFragment 데이터 바인팅 세팅 적용

--- app/src/main/java/com/example/keppthe20220311/fragments/AppointmentListFragment.kt
+++ app/src/main/java/com/example/keppthe20220311/fragments/AppointmentListFragment.kt
@@ -4,21 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.keppthe20220311.R
+import com.example.keppthe20220311.databinding.FragmentAppointmentListBinding
 
-class AppointmentListFragment : Fragment() {
+class AppointmentListFragment : BaseFragment() {
+
+    lateinit var binding: FragmentAppointmentListBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_appointment_list,container,false)
+       binding = DataBindingUtil.inflate(inflater,R.layout.fragment_appointment_list,container,false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        setupEvent()
+        setValues()
 
+    }
+
+    override fun setupEvent() {
+
+    }
+
+    override fun setValues() {
+
+        bin
     }
 }
--- app/src/main/java/com/example/keppthe20220311/fragments/MyProfile_ListFragment.kt
+++ app/src/main/java/com/example/keppthe20220311/fragments/MyProfile_ListFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.keppthe20220311.R
+import com.example.keppthe20220311.api.ServerAPI
 import com.example.keppthe20220311.databinding.FragmentMyProfileBinding
 import com.example.keppthe20220311.datas.BasicResponse
 import com.example.keppthe20220311.utils.ContextUtil
@@ -40,6 +41,7 @@ class MyProfile_ListFragment : BaseFragment() {
     }
 
     override fun setValues() {
+
 
 //        내 정보 조회 > UI 반영
         apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext)).enqueue(object :Callback<BasicResponse>{
