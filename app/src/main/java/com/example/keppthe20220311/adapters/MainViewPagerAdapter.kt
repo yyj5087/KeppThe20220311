c46d230c31a4502b1520536ceb5c5af23e103fdd
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-14 12:05:28 +0900

    뷰페이저 어댑터 완성

---
+++ app/src/main/java/com/example/keppthe20220311/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.keppthe20220311.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.keppthe20220311.fragments.AppointmentListFragment
+import com.example.keppthe20220311.fragments.MyProfile_ListFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount() = 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position){
+            0 -> AppointmentListFragment()
+            else -> MyProfile_ListFragment()
+        }
+    }
+
+
+}
