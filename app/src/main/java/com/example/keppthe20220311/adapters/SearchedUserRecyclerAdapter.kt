346e4cb4e822155c661de6c0542230cb5e81cbce
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-14 17:26:53 +0900

    리싸이클러뷰의 어댑터 제작중

---
+++ app/src/main/java/com/example/keppthe20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.keppthe20220311.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchedUserRecyclerAdapter : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
