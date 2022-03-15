a7ce8a0ce8da6b539f751a99e7b1bdf1ee70b2b2
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-15 10:10:07 +0900

    친구추가 요청 API - 어댑터에서 활용  - 자동 토큰 첨부 활용

--- app/src/main/java/com/example/keppthe20220311/adapters/SearchedUserRecyclerAdapter.kt
+++ app/src/main/java/com/example/keppthe20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -7,12 +7,17 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.keppthe20220311.R
 import com.example.keppthe20220311.api.APIList
 import com.example.keppthe20220311.api.ServerAPI
+import com.example.keppthe20220311.datas.BasicResponse
 import com.example.keppthe20220311.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SearchedUserRecyclerAdapter(
     val mContext: Context,
@@ -27,15 +32,15 @@ class SearchedUserRecyclerAdapter(
         val txtEmail = view.findViewById<TextView>(R.id.txtEmail)
         val btnAddFriend = view.findViewById<Button>(R.id.btnAddFriend)
 
-        fun bind( data: UserData ){
+        fun bind(data: UserData) {
 
 //            실 데이터 반영 기능이 있는 함수
             txtNickname.text = data.nick_name
 
             Glide.with(mContext).load(data.profile_img).into(imgProfile)
 
-            when(data.provider){
-                "default" ->{
+            when (data.provider) {
+                "default" -> {
                     imgSocialLoginLogo.visibility = View.GONE
 
                     txtEmail.text = data.email
@@ -60,11 +65,27 @@ class SearchedUserRecyclerAdapter(
 //            친구 추가 버튼이 눌리면 할 일 => 친구 추가 요청 API 호출.
 //            어댑터에서  =>  API를 호출? => 레트로핏 객체직접 생성해서 호출.
 
-            val retrofit = ServerAPI.getRetrofit(mContext)
-            val apiList = retrofit.create(APIList::class.java)
+            btnAddFriend.setOnClickListener {
 
-       //     apiList.
+                val retrofit = ServerAPI.getRetrofit(mContext)
+                val apiList = retrofit.create(APIList::class.java)
 
+                apiList.postRequestAddFriend(data.id).enqueue(object : Callback<BasicResponse>{
+                    override fun onResponse(
+                        call: Call<BasicResponse>,
+                        response: Response<BasicResponse>
+                    ) {
+                        if (response.isSuccessful){
+                            Toast.makeText(mContext, "친구 요청을 보냈습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                    }
+
+                })
+            }
 
 
         }
@@ -75,7 +96,8 @@ class SearchedUserRecyclerAdapter(
 //        xml을 inflate해와서 => 이를 가지고, MyViewHolder 객체로 생성. 리턴.
 //        재사용성을 알아서 보존해줌
 
-        val row = LayoutInflater.from(mContext).inflate(R.layout.searched_users_list_item, parent, false)
+        val row =
+            LayoutInflater.from(mContext).inflate(R.layout.searched_users_list_item, parent, false)
         return MyViewHolder(row)
     }
 
@@ -90,6 +112,7 @@ class SearchedUserRecyclerAdapter(
         holder.bind(data)
 
     }
-//  몇개의 아이템을 보여줄 예정인지? => 데이터목록의 갯수만큼.
+
+    //  몇개의 아이템을 보여줄 예정인지? => 데이터목록의 갯수만큼.
     override fun getItemCount() = mList.size
 }
