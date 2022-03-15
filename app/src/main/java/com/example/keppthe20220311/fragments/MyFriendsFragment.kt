aae092d6ed617d60352e3750c1f370b88b846d50
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-15 10:50:40 +0900

    서버에서 내 친구 목록 가져와서, ArrayList에 담아두기

--- app/src/main/java/com/example/keppthe20220311/fragments/MyFriendsFragment.kt
+++ app/src/main/java/com/example/keppthe20220311/fragments/MyFriendsFragment.kt
@@ -7,10 +7,17 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.keppthe20220311.R
 import com.example.keppthe20220311.databinding.FragmentMyFriendsBinding
+import com.example.keppthe20220311.datas.BasicResponse
+import com.example.keppthe20220311.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyFriendsFragment : BaseFragment() {
 
     lateinit var binding: FragmentMyFriendsBinding
+
+    val mMyFriendsList = ArrayList<UserData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,5 +44,25 @@ class MyFriendsFragment : BaseFragment() {
 
     override fun setValues() {
 
+        getMyFriendsFromServer()
+    }
+    fun getMyFriendsFromServer(){
+        apiList.getRequestFriendList("my").enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if(response.isSuccessful){
+                    val br = response.body()!!
+
+                    mMyFriendsList.clear()
+
+                    mMyFriendsList.addAll(br.data.friends)
+
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
     }
 }
