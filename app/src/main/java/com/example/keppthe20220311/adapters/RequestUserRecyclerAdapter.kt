d082d9247d099c14dcd6464efb427c1ea125f531
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-15 11:51:01 +0900

    두개의 버튼이 눌리면 할 일을, 하나의 코드로 표현  - tag속성에 각 버튼의 메모를 남겨서 활용

--- app/src/main/java/com/example/keppthe20220311/adapters/RequestUserRecyclerAdapter.kt
+++ app/src/main/java/com/example/keppthe20220311/adapters/RequestUserRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.keppthe20220311.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -16,12 +18,14 @@ class RequestUserRecyclerAdapter(
     val mList: List<UserData>
 ) : RecyclerView.Adapter<RequestUserRecyclerAdapter.MyViewHolder>() {
 
-    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         val imgProfile = view.findViewById<ImageView>(R.id.imgProfile)
         val txtNickname = view.findViewById<TextView>(R.id.txtNickname)
         val imgSocialLoginLogo = view.findViewById<ImageView>(R.id.imgSocialLoginLogo)
         val txtEmail = view.findViewById<TextView>(R.id.txtEmail)
+        val btnAccept = view.findViewById<Button>(R.id.btnAccept)
+        val btnDeny = view.findViewById<Button>(R.id.btnDeny)
 
         fun bing(data: UserData) {
 
@@ -31,10 +35,12 @@ class RequestUserRecyclerAdapter(
 
 //        사용자의 provider - "default" : 이메일 표시, "kakao" : 카카오 로그인, "facebook" : 페북
 //        txtEmail 자리에 반영
-            when(data.provider){
+
+
+            when (data.provider) {
                 "default" -> {
 //                이메일 표시
-                    txtEmail.text  = data.email
+                    txtEmail.text = data.email
 
 //                로고 이미지 숨김
                     imgSocialLoginLogo.visibility = View.GONE
@@ -48,30 +54,49 @@ class RequestUserRecyclerAdapter(
 //                "카카오로그인"
 //                로고 이미지 : 카카오 아이콘
                 }
-                "facebook" ->{
+                "facebook" -> {
                     txtEmail.text = "페북 로그인"
                     Glide.with(mContext).load(R.drawable.ff).into(imgSocialLoginLogo)
 
                 }
-                "naver" ->{
+                "naver" -> {
                     txtEmail.text = "네이버 로그인"
 
 //                Glide는 웹의 이미지 뿐만 아니라, 우리 프로젝트 내부의 이미지도 불러낼수 있다.
                     Glide.with(mContext).load(R.drawable.nn).into(imgSocialLoginLogo)
 
                 }
-                else ->{
+                else -> {
 //                그 외의 잘못된 경우우
                 }
             }
-        }
 
+            //            btnAccept, btnDeny 모두 같은 API롤 호출 (하는 행동이 같다.)
+//            => type 파라미터에 첨부하는 값만 다름. (수락/ 거절)
+
+//            두개의 버튼이 눌리면 할 일을 하나의 변수에 담아두자. (같은 일을 하게)
+//            할 일 : Interface => 정석 : object  : 인터페이스 종류 { }
+//            축약문법 (lambda) => 인터페이스종류  { }
+            val ocl = View.OnClickListener{
+//                서버에 수락  / 거절 의사 전달.
+//                수락버튼 : 수락 / 거절버튼 : 거절 => 어느 버튼을 눌렀는지? 파악 가능해야, 파라미터도 다르게 전달
+//                it 변수 : 클릭된 버튼을 담고 있는 역할.
+//                tag 속성 : 아무 말이나 적어도 되는 일종의 메모. 수락/ 거절 등 보내야할 값을 메모해두자
+
+                val tagStr = it.tag.toString()
+                Log.d("보낼파라미터값", tagStr)
+            }
+
+            btnAccept.setOnClickListener(ocl)
+            btnDeny.setOnClickListener(ocl)
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
-        val view = LayoutInflater.from(mContext).inflate(R.layout.requested_user_list_item,parent,false)
+        val view =
+            LayoutInflater.from(mContext).inflate(R.layout.requested_user_list_item, parent, false)
         return MyViewHolder(view)
     }
 
