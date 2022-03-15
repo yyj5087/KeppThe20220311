e4fb5519e9b94bdc43a62a9564581f4b02d116fa
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-15 10:24:21 +0900

    친구 요청 목록 프래그먼트 클래스 추가

---
+++ app/src/main/java/com/example/keppthe20220311/fragments/RequestedUsersFragment.kt
@@ -0,0 +1,42 @@
+package com.example.keppthe20220311.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.keppthe20220311.R
+import com.example.keppthe20220311.databinding.FragmentMyFriendsBinding
+import com.example.keppthe20220311.databinding.FragmentRequestedUsersBinding
+
+class RequestedUsersFragment : BaseFragment() {
+
+    lateinit var binding: FragmentRequestedUsersBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_requested_users,false)
+        return binding.root
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvent()
+        setValues()
+
+    }
+
+
+
+    override fun setupEvent() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
