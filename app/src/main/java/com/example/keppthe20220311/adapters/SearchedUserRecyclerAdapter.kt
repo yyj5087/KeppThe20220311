c91cf94887f7d769a9bb74a0dfa3e88b5d1242fa
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-15 09:53:10 +0900

    retrofit의 세팅 추가  - 모든 API 호출은, 자동으로 X-HTTP-TOKEN 이름표에 토큰값을 첨부하도록

--- app/src/main/java/com/example/keppthe20220311/adapters/SearchedUserRecyclerAdapter.kt
+++ app/src/main/java/com/example/keppthe20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -10,6 +10,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.keppthe20220311.R
+import com.example.keppthe20220311.api.APIList
+import com.example.keppthe20220311.api.ServerAPI
 import com.example.keppthe20220311.datas.UserData
 
 class SearchedUserRecyclerAdapter(
@@ -54,6 +56,17 @@ class SearchedUserRecyclerAdapter(
                     txtEmail.text = "네이버 로그인"
                 }
             }
+
+//            친구 추가 버튼이 눌리면 할 일 => 친구 추가 요청 API 호출.
+//            어댑터에서  =>  API를 호출? => 레트로핏 객체직접 생성해서 호출.
+
+            val retrofit = ServerAPI.getRetrofit()
+            val apiList = retrofit.create(APIList::class.java)
+
+       //     apiList.
+
+
+
         }
     }
 
