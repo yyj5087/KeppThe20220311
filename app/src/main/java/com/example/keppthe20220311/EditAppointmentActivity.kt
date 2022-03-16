ba5a5e01f847c8c019ef45ed74ff500932efea2d
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-16 16:09:06 +0900

    InfoWindow문서 따라해보기

--- app/src/main/java/com/example/keppthe20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keppthe20220311/EditAppointmentActivity.kt
@@ -13,6 +13,7 @@ import com.example.keppthe20220311.databinding.ActivityEditAppointmentBinding
 import com.example.keppthe20220311.datas.BasicResponse
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.PathOverlay
 import com.odsay.odsayandroidsdk.API
@@ -316,7 +317,15 @@ class EditAppointmentActivity : BaseActivity() {
 
                             val payment = infoObj.getInt("payment")  //소요 비용
 
-                            Toast.makeText(mContext, "시간 :${totalTime}분, 비용 : ${payment}원", Toast.LENGTH_SHORT).show()
+//                            네이버 지도 라이브러리의 InfoWindow 기능 활용
+                            val infoWindow = InfoWindow()
+                            infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(mContext){
+                                override fun getText(p0: InfoWindow): CharSequence {
+                                    return "이동시간 : ${totalTime}분, 비용${payment}원"
+                                }
+
+                            }
+                            infoWindow.open(marker!!)
                         }
 
                         override fun onError(p0: Int, p1: String?, p2: API?) {
