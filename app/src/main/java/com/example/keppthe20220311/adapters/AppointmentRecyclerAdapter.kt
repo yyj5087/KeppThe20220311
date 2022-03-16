8eb3ac392474b8fc2e73b73ef130d6406077b380
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-16 10:55:55 +0900

    약속 목록 어댑터 추가

---
+++ app/src/main/java/com/example/keppthe20220311/adapters/AppointmentRecyclerAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.keppthe20220311.adapters
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.keppthe20220311.R
+import com.example.keppthe20220311.api.APIList
+import com.example.keppthe20220311.api.ServerAPI
+import com.example.keppthe20220311.datas.AppointmentData
+import com.example.keppthe20220311.datas.BasicResponse
+import com.example.keppthe20220311.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class AppointmentRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<AppointmentData>
+) : RecyclerView.Adapter<AppointmentRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+
+
+        fun bing(data: AppointmentData) {
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val view =
+            LayoutInflater.from(mContext).inflate(R.layout.appointment_list_item, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val data = mList[position]
+        holder.bing(data)
+    }
+
+    override fun getItemCount() = mList.size
+
+}
