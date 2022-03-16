9f2a469fce6adf9b0f815037245d5bb7cc1f18ef
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-16 16:51:02 +0900

    지도 띄우기 + 카메라이동 / 마커 설정

--- app/src/main/java/com/example/keppthe20220311/ViewMapActivity.kt
+++ app/src/main/java/com/example/keppthe20220311/ViewMapActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.keppthe20220311.databinding.ActivityViewMapBinding
 import com.example.keppthe20220311.datas.AppointmentData
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.Marker
 
 class ViewMapActivity : BaseActivity() {
 
@@ -29,5 +32,22 @@ class ViewMapActivity : BaseActivity() {
 
     override fun setValues() {
 
-    }
+        binding.naverMapView.getMapAsync {
+            val naverMap = it
+
+//            도착지 자체를를 변수화.
+            val destLatLng = LatLng(mAppointment.latitude, mAppointment.longitude)
+
+//           도착지로 카메라 이동
+            val cameraUpdate = CameraUpdate.scrollTo(destLatLng)
+            naverMap.moveCamera(cameraUpdate)
+
+//            마커도 찍어주자.
+
+            val marker = Marker()
+            marker.position = destLatLng
+            marker.map = naverMap
+
+        }
+   }
 }
