5feefc657efe9caa617ed215da683017f8e806b1
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-17 11:08:34 +0900

    커스텀 액션바 적용

--- app/src/main/java/com/example/keppthe20220311/BaseActivity.kt
+++ app/src/main/java/com/example/keppthe20220311/BaseActivity.kt
@@ -2,7 +2,9 @@ package com.example.keppthe20220311
 
 import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.keppthe20220311.api.APIList
 import com.example.keppthe20220311.api.ServerAPI
 
@@ -21,8 +23,27 @@ abstract class BaseActivity: AppCompatActivity() {
         val retrofit = ServerAPI.getRetrofit(mContext)
         apiList = retrofit.create(APIList::class.java)
 
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
+
+
     }
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    fun setCustomActionBar(){
+
+        val defaultActionBar = supportActionBar!!
+
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        val toolBar = defaultActionBar.customView.parent as Toolbar
+        toolBar.setContentInsetsAbsolute(0,0)
+
+    }
+
 }
