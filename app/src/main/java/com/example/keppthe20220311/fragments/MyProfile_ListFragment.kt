9fb2f7c55cf7308432c8a92a979b1acf22e36bba
win10 <naki5087@naver.com>
2022-03-23 10:44:10 +0900

    사진 선택하기전에, 파일 조회 권한 획득  - 파일 조회 : 코드로 해당 파일의 실제 경로 획득. (서버 첨부 OK)

--- app/src/main/java/com/example/keppthe20220311/fragments/MyProfile_ListFragment.kt
+++ app/src/main/java/com/example/keppthe20220311/fragments/MyProfile_ListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.keppthe20220311.fragments
 
+import android.Manifest
 import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.PermissionChecker
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
@@ -21,6 +23,8 @@ import com.example.keppthe20220311.databinding.FragmentMyProfileBinding
 import com.example.keppthe20220311.datas.BasicResponse
 import com.example.keppthe20220311.utils.ContextUtil
 import com.example.keppthe20220311.utils.URIPathHelper
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -56,14 +60,33 @@ class MyProfile_ListFragment : BaseFragment() {
 
         binding.imgProfile.setOnClickListener {
 
+//            이미지 조회 권한 확인
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+
 //            이미지 선택 화면으로 이동 - 안드로이드 제공 화면 활용 : Intent (4)
 
 //            다른 화면에서 결과 받아오기 - Intent (3) : startActivityForResult
 
-            val myIntent = Intent()
-            myIntent.action = Intent.ACTION_PICK  // 뭔가 가지러 가는 행동이라고 명시
-            myIntent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE // 사진을 가지러 간다고 명시.
-            startActivityForResult(myIntent, REQ_CODE_GALLERY)
+
+                    val myIntent = Intent()
+                    myIntent.action = Intent.ACTION_PICK  // 뭔가 가지러 가는 행동이라고 명시
+                    myIntent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE // 사진을 가지러 간다고 명시.
+                    startActivityForResult(myIntent, REQ_CODE_GALLERY)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "갤러리 조회 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check()
+
+
 
         }
 
